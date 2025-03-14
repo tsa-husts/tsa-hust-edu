d7504c0223d79792645c9fef5049a536d81201ca
Oleme <93308720+vnm2708@users.noreply.github.com>
2025-03-15 01:04:24 +0700

    Update mk.js

--- mk.js
+++ mk.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let savedLogin = null;
 
